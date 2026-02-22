52923cbcdebea099b53a48eb64f5f40cfd00cee6
Astronaut-X-X <307680186@qq.com>
2026-02-22 16:10:59 +0800

    Update Telegram constants to set new AppID and AppHash values while commenting out the previous configurations. This change ensures the Telegram client uses the correct credentials for integration.

--- telegram/const.go
+++ telegram/const.go
@@ -1,9 +1,12 @@
 package telegram
 
 const (
-	AppID   = 206288
-	AppHash = "b18441a1fb7b187f70f1afc7cf2da9ce"
+	// AppID   = 206288
+	// AppHash = "b18441a1fb7b187f70f1afc7cf2da9ce"
 
-// AppID   = 17349
-// AppHash = "344583e45741c457fe1862106095a5eb"
+	// AppID   = 17349
+	// AppHash = "344583e45741c457fe1862106095a5eb"、
+
+	AppID   = 2496
+	AppHash = "8da85b0d5bfe62527e5b244c209159c3"
 )
