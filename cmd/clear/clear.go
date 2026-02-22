991ba400e1785f0960b631497821ffabd1d58294
Astronaut-X-X <307680186@qq.com>
2026-02-22 17:01:20 +0800

    Update clear command to use conditional deletion for download file messages and comments in the database, enhancing data management precision.

--- cmd/clear/clear.go
+++ cmd/clear/clear.go
@@ -12,8 +12,8 @@ func main() {
 		Debug: true,
 	})
 
-	database.DB.Model(&model.DownloadFileMessage{}).Delete(&model.DownloadFileMessage{})
-	database.DB.Model(&model.DownloadFileComment{}).Delete(&model.DownloadFileComment{})
+	database.DB.Model(&model.DownloadFileMessage{}).Where("message_id = ?", "*").Delete(&model.DownloadFileMessage{})
+	database.DB.Model(&model.DownloadFileComment{}).Where("file_index = ?", "*").Delete(&model.DownloadFileComment{})
 
 	fmt.Println("clear download file message and download file comment success")
 }
